093c3558851b6c5bd79b5a1decd90542d5cb5406
Lucas Feitoza <lucas_feitoza@outlook.com>
2023-04-05 16:55:37 -0300

    feat: controller of Client done

--- src/main/java/dio/designpatterns/gof/controller/ClientController.java
+++ src/main/java/dio/designpatterns/gof/controller/ClientController.java
@@ -1,4 +1,42 @@
 package dio.designpatterns.gof.controller;
 
+import dio.designpatterns.gof.model.Client;
+import dio.designpatterns.gof.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("clients")
 public class ClientController {
+
+    @Autowired
+    private ClientService clientService;
+
+    @GetMapping
+    public ResponseEntity<Iterable<Client>> getAll() {
+        return ResponseEntity.ok(clientService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Client> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(clientService.getById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Client> insert(@RequestBody Client client) {
+        return ResponseEntity.ok(client);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Client> update(@PathVariable Long id, @RequestBody Client client) {
+        clientService.update(id, client);
+        return ResponseEntity.ok(client);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        clientService.delete(id);
+        return ResponseEntity.ok().build();
+    }
 }
