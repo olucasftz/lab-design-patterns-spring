7dc17f1b2b15eb030827a2e6811bc91a91d8416b
Lucas Feitoza <lucas_feitoza@outlook.com>
2023-04-05 15:41:39 -0300

    feat: model structured, entities and repositories

---
+++ src/main/java/dio/designpatterns/gof/model/AdressRepository.java
@@ -0,0 +1,7 @@
+package dio.designpatterns.gof.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AdressRepository extends CrudRepository<Adress, String> {
+
+}
